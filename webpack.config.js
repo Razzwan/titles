956b9bdce29b00b32d455920507253b1fd9e998f
Alexandr10m <serbackij@gmail.com>
2020-09-03 17:45:18 +0300

    Чуть откоректировал сборку

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,24 @@
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 const path = require(`path`);
 
 module.exports = {
+  mode: `development`,
   entry: `./src/index.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
+  devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    open: true,
-    inline: true,
-    port: 1337,
-    historyApiFallback: true
+    watchContentBase: true
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: `babel-loader`,
-        },
-      }
+        test: /\.css$/i,
+        use: [`style-loader`, `css-loader`],
+      },
     ],
   },
-  resolve: {
-    extensions: [`.ts`, `.tsx`, `.js`, `json`]
-  },
-  devtool: `source-map`,
 };
