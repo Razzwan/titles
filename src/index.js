963625c9f6d28deeea8cfd32bfc9b6df7d157ed7
Alexandr10m <serbackij@gmail.com>
2020-09-04 16:54:55 +0300

    Добавил сбор неубходимых узлов в коллекцию

--- src/index.js
+++ src/index.js
@@ -1,20 +1,112 @@
 (function () {
-  let currentElement = null;
-  const titlesH1 = document.getElementsByTagName('h1');
 
-  document.addEventListener('keypress', function(element) {
-    if (element.key === 'h') {
-      // 1. снять с предыдущего элемента дополнительные стили с граниецей
-      console.log('Мы нажали кнопку H');
-      if (currentElement) {
-        currentElement.classList.remove('active');
+  const Tag = {
+    H1: `h1`,
+    H2: `h2`,
+    H3: `h3`,
+    H4: `h4`,
+    H5: `h5`,
+    H6: `h6`,
+    A: `a`,
+  }
+
+  const NodeType = {
+    HEADER: `header`,
+    LINK: `link`,
+    LANDMARK: `landmark`,
+  };
+
+  const KeyCode = {
+    H: 72,
+    L: 76,
+    M: 77,
+  };
+
+  const checkTag = (element) => {
+      return Tag[element.tagName] ? true : false;
+  };
+
+  const checkAriaAttr = (element) => {
+    return element.hasAttribute(`role`) ? true : false;
+  };
+
+  const collectNodes = (element) => {
+    const isNeededTag = checkTag(element);
+
+      if (isNeededTag) {
+        nodeContainer.push(element);
       }
+      if (checkAriaAttr(element) && !isNeededTag) {
+        nodeContainer.push(element);
+      }
+      if (element.children.length > 0) {
+        for (let i = 0; i < element.children.length ; i++) {
+          collectNodes(element.children[i])
+        }
+      }
+  };
 
-      // 2. добавть новому элементу границу
-      currentElement = titlesH1.item(0);
-      currentElement.classList.add('active');
+  const findNodeIndex = (start, nodeType) => {
+    start = start === null || start > nodeContainer.length ? 0 : start + 1;
+
+    for (let i = start; i < nodeContainer.length; i++) {
+      if (nodeType === `header` && Tag[nodeContainer[i].tagName]) {
+        if (Tag[nodeContainer[i].tagName] === Tag.A) {
+          continue;
+        }
+        return i;
+        break;
+      }
+      if (nodeType === `header` && !Tag[nodeContainer[i].tagName]) {
+        return nodeContainer.findIndex((node) => Tag[node.tagName]);
+      }
+      if (nodeType === `link` && Tag[nodeContainer[i].tagName] === Tag.A) {
+        return i;
+        break;
+      }
+      if (nodeType === `landmark` && !Tag[nodeContainer[i].tagName]) {
+        return i;
+        break;
+      }
     }
-  });
+  };
 
-  console.log('elements', titlesH1);
+  const removeActiveClass = () => {
+    nodeContainer[currentNodeIndex].classList.remove(`active`);
+  };
+
+  const addActiveClass = (nodeType) => {
+    currentNodeIndex = findNodeIndex(currentNodeIndex, nodeType);
+    if (currentNodeIndex !== -1 ) {
+      nodeContainer[currentNodeIndex].classList.add(`active`);
+    }
+  };
+
+
+  let nodeContainer = [];
+  let currentNodeIndex = null;
+
+  collectNodes(document.body);
+
+  document.addEventListener(`keydown`, (evt) => {
+    if (evt.keyCode === KeyCode.H) {
+      if (currentNodeIndex !== null) {
+        removeActiveClass();
+      }
+      addActiveClass(NodeType.HEADER);
+    }
+    if (evt.keyCode === KeyCode.L) {
+      if (currentNodeIndex !== null) {
+        removeActiveClass();
+      }
+      addActiveClass(NodeType.LINK);
+    }
+    if (evt.keyCode === KeyCode.M) {
+      if (currentNodeIndex !== null) {
+        removeActiveClass();
+      }
+      addActiveClass(NodeType.LANDMARK);
+    }
+
+  });
 })();
