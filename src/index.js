4607408e1174945e95e758e5433a7e288f7cb605
Razzwan <razvanlomov@gmail.com>
2020-09-04 20:07:44 +0300

    Update index.js
    Upgrade the system

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 (function () {
 
-  const Tag = {
+  const Header = {
     H1: `h1`,
     H2: `h2`,
     H3: `h3`,
     H4: `h4`,
     H5: `h5`,
     H6: `h6`,
-    A: `a`,
   }
+  
+  const Link = `a`;
 
   const NodeType = {
     HEADER: `header`,
@@ -21,23 +22,30 @@
     L: 76,
     M: 77,
   };
+  
+  let nodeContainer = [];
+  let currentNodeIndex = null;
 
-  const checkTag = (element) => {
-      return Tag[element.tagName] ? true : false;
+  const checkHeader = (element) => {
+      return Header[element.tagName] ? true : false;
   };
+  
+  const checkLink = (element) => {
+    return Boolean(Link[element.tagName]);
+  }'
 
   const checkAriaAttr = (element) => {
     return element.hasAttribute(`role`) ? true : false;
   };
 
   const collectNodes = (element) => {
-    const isNeededTag = checkTag(element);
 
-      if (isNeededTag) {
-        nodeContainer.push(element);
-      }
-      if (checkAriaAttr(element) && !isNeededTag) {
-        nodeContainer.push(element);
+      if (checkHeader(element)) {
+        nodeContainer.push([element, NodeType.HEADER]);
+      } else if (checkLink(element)) {
+        nodeContainer.push([element, NodeType.LINK]);
+      } else if (checkAriaAttr(element)) {
+        nodeContainer.push([element, NodeType.LANDMARK]);
       }
       if (element.children.length > 0) {
         for (let i = 0; i < element.children.length ; i++) {
@@ -47,59 +55,35 @@
   };
 
   const findNodeIndex = (start, nodeType) => {
-    start = start === null || start > nodeContainer.length ? 0 : start + 1;
+    const newIndex = start === null || start > nodeContainer.length ? 0 : start + 1;
 
     for (let i = start; i < nodeContainer.length; i++) {
-      if (nodeType === `header` && Tag[nodeContainer[i].tagName]) {
-        if (Tag[nodeContainer[i].tagName] === Tag.A) {
-          continue;
-        }
+      if (nodeContainer[i][1] === nodeType) {
         return i;
-        break;
-      }
-      if (nodeType === `link` && Tag[nodeContainer[i].tagName] === Tag.A) {
-        return i;
-        break;
-      }
-      if (nodeType === `landmark` && !Tag[nodeContainer[i].tagName]) {
-        return i;
-        break;
       }
     }
-  };
-
-  const removeActiveClass = () => {
-    nodeContainer[currentNodeIndex].classList.remove(`active`);
+    return undefined;
   };
 
   const addActiveClass = (nodeType) => {
-    currentNodeIndex = findNodeIndex(currentNodeIndex, nodeType);
+    const newIndex = findNodeIndex(currentNodeIndex, nodeType);
+    if (typeof currentNodeIndex === 'number') {
+      currentNodeIndex = newIndex;
+      // TODO: удалить ВСЕ КЛАССЫ active из ВСЕХ ЭЛЕМЕНТОВ
       nodeContainer[currentNodeIndex].classList.add(`active`);
+    }
   };
-
-
-  let nodeContainer = [];
-  let currentNodeIndex = null;
 
   collectNodes(document.body);
 
   document.addEventListener(`keydown`, (evt) => {
     if (evt.keyCode === KeyCode.H) {
-      if (currentNodeIndex !== null) {
-        removeActiveClass();
-      }
       addActiveClass(NodeType.HEADER);
     }
     if (evt.keyCode === KeyCode.L) {
-      if (currentNodeIndex !== null) {
-        removeActiveClass();
-      }
       addActiveClass(NodeType.LINK);
     }
     if (evt.keyCode === KeyCode.M) {
-      if (currentNodeIndex !== null) {
-        removeActiveClass();
-      }
       addActiveClass(NodeType.LANDMARK);
     }
 
